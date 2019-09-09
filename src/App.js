6ebc644c5c2e2781858bbf52ec0af5ef9fc11c56
Fabio <fabiosjordao@gmail.com>
2019-09-09 11:15:35 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import appActions from './actions/appActions'
+
+class Children extends Component {
+    setText() {
+        this.props.dispatch(appActions.setText('Aquele texto'))
+    }
+    render() {
+        return (
+            <Fragment>
+                <button onClick={() => this.setText()}>Mudar texto</button>
+            </Fragment>
+        )
+    }
+}
+
+const ChildrenConnected = connect(store => ({ text: store.text }))(Children)
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>{this.props.text}</h1>
+                <ChildrenConnected />
+            </Fragment>
+        )
+    }
+}
+
+export default connect(store => ({ text: store.text }))(App)
